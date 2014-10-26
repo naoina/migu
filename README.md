b614ebee35f645b5781759e92e7c7290d257fdcc
Naoya Inada <naoina@kuune.org>
2014-10-27 00:45:31 +0900

    Add command-line interface

--- README.md
+++ README.md
@@ -8,12 +8,64 @@ This tool is inspired by [Ridgepole](https://github.com/winebarrel/ridgepole).
 
 ## Installation
 
-    # As library
-    go get -u github.com/naoina/migu
+    go get -u github.com/naoina/migu/cmd/migu
 
 ## Usage
 
-See files in `_example/simple/`.
+Save the following Go code as `schema.go`:
+
+```go
+package yourownpackagename
+
+type User struct {
+	Name string
+	Age  int
+}
+```
+
+Then type the following commands to execute the first migration:
+
+```
+% mysqladmin create migu_test
+% migu -u root sync migu_test schema.go
+% mysql -u root migu_test -e 'desc user'
++-------+--------------+------+-----+---------+-------+
+| Field | Type         | Null | Key | Default | Extra |
++-------+--------------+------+-----+---------+-------+
+| name  | varchar(255) | NO   |     | NULL    |       |
+| age   | int(11)      | NO   |     | NULL    |       |
++-------+--------------+------+-----+---------+-------+
+```
+
+`migu sync` command will create the table `user` into database `migu_test` because it still not exist.
+
+Next, modify and save `schema.go` as follows:
+
+```go
+package yourownpackagename
+
+type User struct {
+	Name string
+	Age  uint
+}
+```
+
+Then type the following commands again:
+
+```
+% migu -u root sync migu_test schema.go
+% mysql -u root migu_test -e 'desc user'
++-------+------------------+------+-----+---------+-------+
+| Field | Type             | Null | Key | Default | Extra |
++-------+------------------+------+-----+---------+-------+
+| name  | varchar(255)     | NO   |     | NULL    |       |
+| age   | int(10) unsigned | NO   |     | NULL    |       |
++-------+------------------+------+-----+---------+-------+
+```
+
+A type of field `age` on `user` table has been changed because type of `Age` in `schema.go` was changed from `int` to `uint`.
+
+See `migu --help` for more options.
 
 ## Supported database
 
@@ -21,7 +73,6 @@ See files in `_example/simple/`.
 
 ## TODO
 
-* Command-line interface
 * Struct Tag support for some ORM
 * PostgreSQL and SQLite3 support
 
---
+++ cmd/migu/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/howeyc/gopass"
+	"github.com/jessevdk/go-flags"
+	"github.com/naoina/migu"
+)
+
+var (
+	progName = filepath.Base(os.Args[0])
+	option   struct {
+		User     string `short:"u" long:"user"`
+		Host     string `short:"h" long:"host"`
+		Password string `short:"p" long:"password" optional:"true" optional-value:"\x00"`
+		Help     bool   `long:"help"`
+
+		cmd    string
+		file   string
+		dbname string
+	}
+)
+
+type usageError struct {
+	error
+}
+
+func usage(code int) {
+	fmt.Fprintf(os.Stderr, `Usage: %s [OPTIONS] COMMAND DATABASE [FILE]
+
+Commands:
+  sync      synchronize the database schema
+
+Options:
+  -u, --user=NAME        User for login to database if not current user
+  -h, --host=HOST        Connect to host of database
+  -p, --password[=PASS]  Password to use when connecting to server.
+                         If password is not given, it's asked from the tty
+      --help             Display this help and exit
+
+With no FILE, or when FILE is -, read standard input.
+
+`, progName)
+	os.Exit(code)
+}
+
+func run(args []string) error {
+	switch len(args) {
+	case 0, 1:
+		return &usageError{
+			error: fmt.Errorf("too few arguments"),
+		}
+	case 2:
+		option.cmd, option.dbname = args[0], args[1]
+	case 3:
+		option.cmd, option.dbname, option.file = args[0], args[1], args[2]
+	default:
+		return &usageError{
+			error: fmt.Errorf("too many arguments"),
+		}
+	}
+	db, err := database()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	switch option.cmd {
+	case "sync":
+		return sync(db)
+	default:
+		return &usageError{
+			error: fmt.Errorf("unknown command: %s", option.cmd),
+		}
+	}
+}
+
+func sync(db *sql.DB) error {
+	var src io.Reader
+	switch option.file {
+	case "", "-":
+		option.file = ""
+		src = os.Stdin
+	}
+	return migu.Sync(db, option.file, src)
+}
+
+func database() (db *sql.DB, err error) {
+	if option.User == "" {
+		if option.User = os.Getenv("USERNAME"); option.User == "" {
+			if option.User = os.Getenv("USER"); option.User == "" {
+				return nil, fmt.Errorf("user is not specified and current user cannot be detected")
+			}
+		}
+	}
+	dsn := bytes.NewBufferString(option.User)
+	if option.Password != "" {
+		if option.Password == "\x00" {
+			fmt.Printf("Enter password: ")
+			option.Password = string(gopass.GetPasswd())
+		}
+		dsn.WriteByte(':')
+		dsn.WriteString(option.Password)
+	}
+	if dsn.Len() > 0 {
+		dsn.WriteByte('@')
+	}
+	if option.Host != "" {
+		dsn.WriteString("tcp(")
+		dsn.WriteString(option.Host)
+		dsn.WriteByte(')')
+	}
+	dsn.WriteByte('/')
+	dsn.WriteString(option.dbname)
+	return sql.Open("mysql", dsn.String())
+}
+
+func main() {
+	parser := flags.NewNamedParser(progName, flags.PrintErrors|flags.PassDoubleDash|flags.PassAfterNonOption)
+	if _, err := parser.AddGroup("", "", &option); err != nil {
+		panic(err)
+	}
+	args, err := parser.Parse()
+	if err != nil {
+		usage(1)
+	}
+	if option.Help {
+		usage(0)
+	}
+	if err := run(args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", progName, err)
+		if _, ok := err.(*usageError); ok {
+			usage(1)
+		} else {
+			os.Exit(1)
+		}
+	}
+}
